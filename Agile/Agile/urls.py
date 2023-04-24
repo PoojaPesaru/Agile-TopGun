f82440ca19a7b1fbf4e29c33fe09663e7e7c5a42
Harsh <124744875+HarshPatel407@users.noreply.github.com>
2023-04-24 19:53:25 -0400

    Add files via upload

--- Agile/Agile/urls.py
+++ Agile/Agile/urls.py
@@ -14,11 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
+import Agile_Project.views
 
-from Agile_Project import views
 urlpatterns = [
-    path('add-task/', views.add_task, name='add_task'),
-    path('task-list/', views.task_list, name='task_list'),
-    path('document-list/', views.document_list, name='document_list'),
-    path('upload-document/', views.upload_document, name='upload_document'),
+    path('', Agile_Project.views.dashboard, name='dashboard'),
+    path('task-list/', Agile_Project.views.task_list, name='task_list'),
+    path('add-task/', Agile_Project.views.add_task, name='add_task'),
+    path('material-tracking/', Agile_Project.views.material_tracking, name='material_tracking'),
+    path('add-material/', Agile_Project.views.add_material, name='add_material'),
+    path('document-list/', Agile_Project.views.add_task, name='document_list')
 ]
+
