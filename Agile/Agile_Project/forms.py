72d5e4ca4fb61406be8cee07d87e695e037cabf6
Arya-Shah <shaharya5152@gmail.com>
2023-04-25 17:29:29 -0400

    Modified Forms and Models.

--- Agile/Agile_Project/forms.py
+++ Agile/Agile_Project/forms.py
@@ -1,13 +1,17 @@
 from django import forms
 from .models import Task
-from .models import Document
 
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ['name', 'description', 'owner', 'status', 'due_date', 'time']
-
-class DocumentForm(forms.ModelForm):
-    class Meta:
-        model = Document
-        fields = ['title','file']
+        fields = ['employee_name', 'client_name', 'location', 'task_name', 'time']
+        labels = {
+            'employee_name': 'Employee Name',
+            'client_name': 'Client Name',
+            'location': 'Location',
+            'task_name': 'Task',
+            'time': 'Time'
+        }
+        widgets = {
+            'time': forms.DateInput(attrs={'type': 'date'})
+        }
