320295da76a3a31f1d6e4e03f61e63e5a672f73c
NavyaJonnala <123429347+NavyaJonnala@users.noreply.github.com>
2023-04-25 11:56:29 -0400

    Add files via upload

--- Agile/Agile_Project/views.py
+++ Agile/Agile_Project/views.py
@@ -7,9 +7,47 @@ from .models import Task
 #from .forms import DocumentForm
 from .models import Document
 
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+
+#user registration view
+def registration_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login') # Replace 'login' with the URL name of your app's login page
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration.html', {'form': form})
+
+#login view
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request=request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('dashboard') # redirect without argument
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
 #dashboard view
 def dashboard(request):
-    return render(request, 'dashboard.html')
+    user = request.user # access the user object using request.user
+    return render(request, 'dashboard.html', {'user': user})
+
+
+#logout view
+def logout_view(request):
+    logout(request)
+    return redirect('dashboard') # Replace 'login' with the URL name of your app's login page
 
 #Material Tracking
 def material_tracking(request):
